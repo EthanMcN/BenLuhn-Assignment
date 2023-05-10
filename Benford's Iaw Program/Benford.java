f65a8d4ad2c1dfe94f7c139379116d1e5635db61
Ethan McNalley <ethanmcnalley13@gmail.coml>
2023-05-10 00:26:13 -0400

    Added more methods

--- Benford's Iaw Program/Benford.java
+++ Benford's Iaw Program/Benford.java
@@ -2,7 +2,48 @@
 
 // Importations needed for the program to function
 import java.util.Scanner;
-import java.io.File;
+//import java.io.File;
 
+class BenfordsLaw{
+
+    public static void main(String[]args){
+
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
+    }
+
+    public static void printMenu(){ 
+        /*
+        * Displays a menu for the user. Lists the numbers the user needs 
+        * to input to access the functions of the sales system.
+        */
+        System.out.println(/* Customer and Sales System\n
+    1. Load Sales File\n
+    2. Analyze File\n
+    3. Generate Graph\n
+    9. Quit\n
+    Enter menu option (1-9) */);
+    }
+
+/*
+    public static int analyzeFile(){
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        int num5 = 0;
+        int num6 = 0;
+        int num7 = 0;
+        int num8 = 0;
+        int num9 = 0;
+        int length = 0;
+        
+
+
+    }
+    */
+    
+}
 
 
