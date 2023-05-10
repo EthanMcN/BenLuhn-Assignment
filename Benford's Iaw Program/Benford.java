ccb71dbc3a490412bb98021f6fcc35564960ebf9
Ethan McNalley <ethanmcnalley13@gmail.coml>
2023-05-10 14:02:30 -0400

    More code for analyzation

--- Benford's Iaw Program/Benford.java
+++ Benford's Iaw Program/Benford.java
@@ -2,7 +2,11 @@
 
 // Importations needed for the program to function
 import java.util.Scanner;
+//import java.lang.Math.*;
+//import java.text.DecimalFormat;
+//import javafx.application.Application;
 //import java.io.File;
+
 
 class BenfordsLaw{
 
@@ -39,10 +43,36 @@ class BenfordsLaw{
         int num9 = 0;
         int length = 0;
         
+        // For loops that analyze the file in order to obtain the first digits
+        for (int line )
 
+        double 1st_percent = Math.round(num1/length*100, 2)
+        double 2nd_percent = Math.round(num2/length*100, 2)
+        double 3rd_percent = Math.round(num3/length*100, 2)
+        double 4th_percent = Math.round(num4/length*100, 2)
+        double 5th_percent = Math.round(num5/length*100, 2)
+        double 6th_percent = Math.round(num6/length*100, 2)
+        double 7th_percent = Math.round(num7/length*100, 2)
+        double 8th_percent = Math.round(num8/length*100, 2)
+        double 9th_percent = Math.round(num9/length*100, 2)
 
+        all_percentages = {1st_percent, 2nd_percent, 3rd_percent, 4th_percent, 5th_percent, 6th_percent, 7th_percent, 8th_percent, 9th_percent};
+        
+        // Benfor's Law application
+        if 29 <= 1st_percent <= 32{
+            println("1: " + 1st_percent + "2: " + 2nd_percent + "3: " + 3rd_percent + "4: " + 4th_percent + "5: " + 5th_percent + "6: " + 6th_percent + "7: " + 7th_percent + "8: " + 8th_percent + "9: " + 9th_percent)
+            println("The data suggests that fraud did not occur.")
+        }
+        else{
+            println("1: " + 1st_percent + "2: " + 2nd_percent + "3: " + 3rd_percent + "4: " + 4th_percent + "5: " + 5th_percent + "6: " + 6th_percent + "7: " + 7th_percent + "8: " + 8th_percent + "9: " + 9th_percent)
+            println("The data suggest that there was fraud present.")
+        }
+
+        return all_percentages
     }
     */
+
+    
     
 }
 
