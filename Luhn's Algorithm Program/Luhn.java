bed2a3cf7b52da855529c275641412da73e2a4ce
QadeerKhann <132603989+QadeerKhann@users.noreply.github.com>
2023-05-09 20:33:54 -0400

    Update Luhn.java

--- Luhn's Algorithm Program/Luhn.java
+++ Luhn's Algorithm Program/Luhn.java
@@ -1,4 +1,8 @@
-// Luhn's algorithm in java
-
-// Imports Scanner into Java
-import java.util.Scanner;
+class Credit_Validation {
+    public static void main (String[] args) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum_total = 0;
+        
+        // Reverses the card number that the user has entered
+        String cardnum = "1234567890";
