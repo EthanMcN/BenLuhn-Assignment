ed941fc5a0200e15275b04f60672c291cc9ec460
QadeerKhann <132603989+QadeerKhann@users.noreply.github.com>
2023-05-10 21:31:53 -0400

    Update Luhn.java

--- Luhn's Algorithm Program/Luhn.java
+++ Luhn's Algorithm Program/Luhn.java
@@ -6,3 +6,20 @@ class Credit_Validation {
         
         // Reverses the card number that the user has entered
         String cardnum = "1234567890";
+
+        String reversednum = cardnum;
+ 
+        StringBuilder input1 = new StringBuilder();
+ 
+        // append a string into StringBuilder input1
+        input1.append(reversednum);
+ 
+        // reverse StringBuilder input1
+        input1.reverse();
+
+        // Odd Digits
+        for (int i = 0; i < String.valueOf(reversednum).length(); i+=2){
+            int oddNumber = (reversednum.charAt(i));
+            sum1 += oddNumber;
+        }
+        
