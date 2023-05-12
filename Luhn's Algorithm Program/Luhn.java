da2823d01f8c59097eabad47f419b32357b801ba
QadeerKhann <132603989+QadeerKhann@users.noreply.github.com>
2023-05-12 09:56:20 -0400

    Update Luhn.java
    added doc strings

--- Luhn's Algorithm Program/Luhn.java
+++ Luhn's Algorithm Program/Luhn.java
@@ -1,4 +1,16 @@
 class Credit_Validation {
+    /*
+    The Luhn algorithm test is used as a credit card digit test for many companies. 
+    It helps determine if a user accidently enters an incorrect credit card number.
+    To valid an existing credit card:
+    Reverse the order of the digits in the number.
+    Perform a partial sum of the odd digits (digits 1, 3, etc) – sum1
+    Take the second, fourth ... and every other even digits in the reversed digits
+    Multiply each digit by two and sum the digits, if the answer is greater than 9 
+    then add the 2 digits to form partial sums for the even digits
+    Sum the partial sums of the even digits to form sum2
+    If sum1 + sum2 ends in zero then the original number is valid, otherwise it is invalid.
+    */
     public static void main (String[] args) {
         int sum1 = 0;
         int sum2 = 0;
@@ -42,6 +54,11 @@ class Credit_Validation {
         }
     }
 }
+
 class Data{
+    /*
+    Prints data
+    */
+    // Prints Data
     System.out.println(data);
 }
